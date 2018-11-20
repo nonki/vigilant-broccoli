7e3db86c3b57877b96379caa6df0d07574237d19
nonki <thomas@flack.it>
2018-11-20 23:14:13 +0000

    Framework for app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    console.log('here');
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Games</h1>
+        <Game game="0" />
+        <Game game="1" />
+        <Game game="2" />
       </div>
     );
+  }
+}
+
+const api = 'http://localhost:8080/keys'
+const getGame = (id) =>
+  fetch(`${api}/${id}`)
+    .then(res =>
+      res.ok ? res.json() : Promise.reject("Failed to get something useful")
+    )
+    .then(apiData => {
+      return apiData
+    });
+
+
+class Game extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { game: null }
+  }
+
+  _getGame(id) {
+    console.log('something');
+    getGame(id).then(game => this.setState({ game }));
+  }
+
+  componentDidMount() {
+    this._getGame(this.props.game)
+  }
+
+  render() {
+    return(
+      <div className="Game" game={this.props.gameId}>
+        {JSON.stringify(this.state.game)}
+      </div>
+    )
   }
 }
 
