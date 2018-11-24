d4feab7f07b946156b3b767bf18c0078dd1f62ff
nonki <thomas@flack.it>
2018-11-24 18:49:12 +0000

    Move birthdaymessage to its own component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import Auth from './Auth.js';
+import BirthdayMessage from './BirthdayMessage.js';
 import Games from './Games.js';
 import { createMuiTheme, MuiThemeProvider  } from '@material-ui/core/styles';
 import pink from '@material-ui/core/colors/pink';
-import purple from '@material-ui/core/colors/purple';
-import Typography from '@material-ui/core/Typography';
+import red from '@material-ui/core/colors/red';
 
 const theme = createMuiTheme({
   palette: {
-    primary: pink,
-    secondary: purple,
+    primary: {
+      main: pink[50],
+    },
+    secondary: {
+      main: red[50],
+    },
   },
 });
 
@@ -51,24 +55,6 @@ class App extends Component {
       </MuiThemeProvider>
     );
   }
-}
-
-
-const BirthdayMessage = (props) => {
-  return (
-    <div className="BirthdayMessage">
-      <Typography color="primary" variant="h2" className="Title">
-        Happy Birthday!
-      </Typography>
-      <div className="Blurb">
-        <Typography color="primary" variant="body1" className="part1">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras convallis pharetra elit. Vestibulum pharetra quam in lectus lobortis sollicitudin vitae eget purus. Praesent vitae ipsum eu elit pharetra aliquam eu vel est. Nullam eget sapien neque. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur vel gravida velit. Morbi volutpat, lectus quis egestas dapibus, libero diam ultrices quam, quis sagittis quam libero sit amet erat. Aenean convallis viverra nisl sed aliquet. In hac habitasse platea dictumst. Praesent ultrices ante bibendum ante suscipit, sed rutrum velit aliquam.
-          <br />
-          Pellentesque suscipit sem a quam auctor venenatis. Phasellus velit arcu, tristique et eros eget, euismod blandit ipsum. Donec sapien felis, fringilla in auctor non, faucibus eget massa. Duis consectetur, massa id fringilla tincidunt, sapien est condimentum dui, et vestibulum dui mauris id lectus. In mattis laoreet mauris, vestibulum vulputate erat rhoncus nec. Etiam nisi est, vulputate nec sapien ac, efficitur ultrices risus. Maecenas elementum fermentum hendrerit. Fusce id dui eget est aliquet elementum. Integer vel ultrices justo.
-        </Typography>
-      </div>
-    </div>
-  )
 }
 
 export default App;
---
+++ src/BirthdayMessage.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { withStyles  } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+const styles = theme => ({
+  container: {
+    padding: theme.spacing.unit,
+    textAlign: 'center',
+    marginBottom: '40px',
+    backgroundColor: theme.palette.primary.main,
+    boxShadow: '0px 6px 24px -1px rgba(30,30,30,0.4)',
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignContent: 'center',
+  },
+  title: {
+    width: '100%',
+    paddingBottom: '50px',
+    paddingTop: '70px',
+  },
+  blurb: {
+    paddingLeft: '10%',
+    paddingRight: '10%',
+    paddingBottom: '30px',
+    color: theme.palette.secondary,
+  }
+});
+
+const BirthdayMessage = (props) => {
+  const { classes } = props;
+
+  return (
+    <div className={classes.container}>
+      <Typography color="textPrimary" variant="h2" className={classes.title}>
+        Happy Birthday!
+      </Typography>
+      <div className={classes.blurb}>
+        <Typography variant="body1">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras convallis pharetra elit. Vestibulum pharetra quam in lectus lobortis sollicitudin vitae eget purus. Praesent vitae ipsum eu elit pharetra aliquam eu vel est. Nullam eget sapien neque. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur vel gravida velit. Morbi volutpat, lectus quis egestas dapibus, libero diam ultrices quam, quis sagittis quam libero sit amet erat. Aenean convallis viverra nisl sed aliquet. In hac habitasse platea dictumst. Praesent ultrices ante bibendum ante suscipit, sed rutrum velit aliquam.
+          <br />
+          Pellentesque suscipit sem a quam auctor venenatis. Phasellus velit arcu, tristique et eros eget, euismod blandit ipsum. Donec sapien felis, fringilla in auctor non, faucibus eget massa. Duis consectetur, massa id fringilla tincidunt, sapien est condimentum dui, et vestibulum dui mauris id lectus. In mattis laoreet mauris, vestibulum vulputate erat rhoncus nec. Etiam nisi est, vulputate nec sapien ac, efficitur ultrices risus. Maecenas elementum fermentum hendrerit. Fusce id dui eget est aliquet elementum. Integer vel ultrices justo.
+        </Typography>
+      </div>
+    </div>
+  )
+}
+
+export default withStyles(styles)(BirthdayMessage);
