b7938c21c4af3423ef993e26ccde51b529bb2b44
nonki <thomas@flack.it>
2018-11-27 23:42:25 +0000

    remove box shadow from youtube video list;

--- src/YoutubeList.js
+++ src/YoutubeList.js
@@ -17,7 +17,6 @@ const styles = theme => ({
     flexWrap: 'nowrap',
     // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
     transform: 'translateZ(0)',
-    boxShadow: theme.shadows[15],
   },
 });
 
